d2906982cc8161daa70c09c8b582bc9e3a6ffb58
scin <fuck@git.hub>
2020-11-21 12:32:23 +0000

    remove test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-var ping = require('ping');
- 
-var hosts = ['Backup-Server.home.robinsmediateam.dev', 'eminem.home.robinsmediateam.dev', 'yahoo.com'];
-hosts.forEach(function(host){
-    ping.sys.probe(host, function(isAlive){
-        var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
-        console.log(msg);
-    });
-});
