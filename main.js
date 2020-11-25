fe1d91ca4da66956fa538abed093b0ad6a151c2d
scinorandex <scinorandex@gmail.com>
2020-11-25 18:46:30 +0800

    added error handling

--- main.js
+++ main.js
@@ -1,30 +1,25 @@
 //RMTBot V2 by Scinorandex for RobinsMediaTeam
 
-//Load all libraries going to be used
 const fs = require('fs');
 const Discord = require('discord.js');
 const https = require("https");
 const ping = require('ping');
 
-//define client and get token
 const client = new Discord.Client();
 const { token } = require('./token.json');
 
-//create global channel and message variable
 var channel;
 var message;
 
-//load JSON files
 const rawListOfServices = fs.readFileSync('services.json');
 const rawListOfIPs = fs.readFileSync('ip.json');
 const servicesJSON = JSON.parse(rawListOfServices);
 const ipJSON = JSON.parse(rawListOfIPs);
 
-const interval = 15; //define interval in minutes
-const intervalInSeconds = interval*60; //define in secods
-const env = "prod"; //define what type of environment, can either be prod or test
+const interval = 15;
+const intervalInSeconds = interval*60;
+const env = "prod"; //prod or test
 
-//initialize list variables
 var globalWorkingServices = [];
 var globalNotWorkingServices = [];
 var globalNeutralServices = [];
@@ -33,7 +28,6 @@ var globalNotWorkingIPs = [];
 var globalImportantNames = ["MainAD", "FalloverAD", "CDN", "Backup Server"];
 var globalImportantDownNames = [];
 
-//initialize and set environment variables based on env
 var masterUser;
 var channelID;
 
@@ -45,10 +39,8 @@ if(env=="prod"){
     masterUser="205911447186702336";
 }
 
-//define list of important services
-var importantServices=["MainAD", "FalloverAD", "CDN", "Backup Server"];
+var globalImportantNames=["MainAD", "FalloverAD", "CDN", "Backup Server"];
 
-//returns a date in a neat format
 function getADate(){
     datetime = new Date();
     da=datetime.getDate();
@@ -75,7 +67,6 @@ function createMessageLists(emoji, indicator, arrayToBeChecked){
     message+="> " + emoji + "**" + indicator + ": **";
     console.log(indicator, arrayToBeChecked);
         for(i = 0; i < arrayToBeChecked.length; i++){
-            //ifelse that adds newline if it's the end of the array
             if (i == arrayToBeChecked.length - 1){
                 message+=arrayToBeChecked[i]+"\n"
               }else{
@@ -86,10 +77,9 @@ function createMessageLists(emoji, indicator, arrayToBeChecked){
 }
 
 function outputToDiscord(){
-    //resets message variable and get a new date
     actualDate=getADate();
     
-    //reset message, areImportantServicesDown, pingMasterUser, and globalImportantDownServices
+    //reset variables
     message="";
     areImportantServicesDown=false;
     pingMasterUser=false;
@@ -103,7 +93,6 @@ function outputToDiscord(){
     globalImportantDownNames = importantDownServices.concat(importantDownIPs);
     if (globalImportantDownNames != 0){areImportantServicesDown = true; pingMasterUser = true};
 
-    //begin structuring the message
     title="**RMT Service Status check for: "+actualDate+"**\n";
     createMessageLists(":white_check_mark:", "Working Services", globalWorkingServices);
     createMessageLists(":x:", "Broken Services", globalNotWorkingServices);
@@ -112,7 +101,6 @@ function outputToDiscord(){
     createMessageLists(":white_check_mark:", "Alive Machines", globalWorkingIPs);
     createMessageLists(":x:", "Dead Machines", globalNotWorkingIPs);
 
-    //add message if important services have been detected as down
     if (areImportantServicesDown==true){
         message += "\n"
         message += "> :x:**Important services that have been detected as down: **\n> "
@@ -125,7 +113,6 @@ function outputToDiscord(){
         }
     }
 
-    //add message if more than 3 machines down
     if (globalNotWorkingIPs.length >= 4){
         pingMasterUser=true;
         message+="\n **More than 4 machines have been detected as offline**\n";
@@ -133,7 +120,6 @@ function outputToDiscord(){
 
     message+="**Done**";
 
-    //send message in an embed and ping masterUser if true
     channel.send(new Discord.MessageEmbed()
         .addField(title,message)
     );
@@ -147,9 +133,9 @@ function checkIfIPisReachable(name, domain){
     ping.sys.probe(domain, function(isAlive){
         console.log(domain, isAlive);
         if(isAlive==true){
-          globalWorkingIPs.push(name); //push name to globalWorkingIPs
+          globalWorkingIPs.push(name);
         }else if(isAlive==false){
-          globalNotWorkingIPs.push(name); //push name to globalNotWorkingIPs
+          globalNotWorkingIPs.push(name);
         }
       });
 }
@@ -157,42 +143,43 @@ function checkIfIPisReachable(name, domain){
 function checkIfIPsAreReachable(){
     for (i = 0; i < ipJSON.length; i++){
 
-        //get parameters from JSON
         name = ipJSON[i].name;
         domain = ipJSON[i].domain;
         checkIfIPisReachable(name, domain);
         
-        //call outputToDiscord when for loop has reached the end of the array
         if(i == ipJSON.length -1){
-            setTimeout(outputToDiscord,10*1000) //wait for 10 seconds then call outputToDiscord
+            setTimeout(outputToDiscord,10*1000)
         }
     }
 }
 
 function checkIfServiceIsRunning(name, domain, port){
     //Get http code of domain and pass to if statement to determine which array to push name to
-    https.get({host: domain}, function(res){
-        if(res.statusCode == 200 || res.statusCode == 301 || res.statusCode == 302){ //kanboard for some reason reports a code 302
-          globalWorkingServices.push(name);     //push name to globalWorkingServices
-        }
-        else if(res.statusCode == 404){
-          globalNotWorkingServices.push(name); //push name to globalNotWorkingServices
-        }else{
-          globalNeutralServices.push(name);   //push name to globalNeutralServices
-          console.log(name, "reported", res.statusCode); //log it in console what the error code is
-        }
-      });
+    try{
+        https.get({host: domain}, function(res){
+            if(res.statusCode == 200 || res.statusCode == 301 || res.statusCode == 302){ //kanboard for some reason reports a code 302
+              globalWorkingServices.push(name);
+            }
+            else if(res.statusCode == 404){
+              globalNotWorkingServices.push(name); 
+            }else{
+              globalNeutralServices.push(name);
+              console.log(name, "reported", res.statusCode);
+            }
+          });
+    }catch{
+        console.log("An https GET request for " + name + " has timed out.")
+    }
 }
 
 function checkIfServicesAreRunning(){
-    //reset all arrays to empty
+    //reset all variables to default
     globalWorkingServices = [];
     globalNotWorkingServices = [];
     globalNeutralServices = [];
     globalWorkingIPs = [];
     globalNotWorkingIPs = [];
 
-    //start looping and calling checkIfServiceIsRunning
     for (i = 0; i < servicesJSON.length; i++){
 
         //get parameters from JSON
@@ -201,14 +188,12 @@ function checkIfServicesAreRunning(){
         port = servicesJSON[i].port;
         checkIfServiceIsRunning(name, domain, port);
 
-        //call checkIfIPsAreReachable when for loop has reached the end of the array
         if(i == servicesJSON.length - 1){
             checkIfIPsAreReachable();
         }
     }
 }
 
-//discord code that starts the timer
 client.on('ready', () => {
     channel = client.channels.cache.get(channelID);
     checkIfServicesAreRunning();
@@ -216,5 +201,4 @@ client.on('ready', () => {
     
 })
 
-//login to discordAPI
 client.login(token);
