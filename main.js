a17570879ea9168d032e48f51bab6b983bbd3e38
scinorandex <scinorandex@gmail.com>
2021-01-08 08:19:43 +0800

    Overhaul bot. v3

--- main.js
+++ main.js
@@ -1,58 +1,44 @@
-//RMTBot V2 by Scinorandex for RobinsMediaTeam
+// RMTBot v3 by Scinorandeex
+// For use with the RobinsMediaTeam Network
+// https://scin.robinsmediateam.dev
 
+// Load all necessary libraries in
 const fs = require('fs');
 const Discord = require('discord.js');
 const https = require("https");
 const ping = require('ping');
+const sqlite3 = require('sqlite3');
 
+// Login to Discord
 const client = new Discord.Client();
 const { token } = require('./token.json');
 
-var channel;
-var message;
-
-const rawListOfServices = fs.readFileSync('services.json');
-const rawListOfIPs = fs.readFileSync('ip.json');
-const servicesJSON = JSON.parse(rawListOfServices);
-const ipJSON = JSON.parse(rawListOfIPs);
-
-const interval = 15;
-const intervalInSeconds = interval*60;
-const env = "prod"; //prod or test
-
-var globalWorkingServices = [];
-var globalNotWorkingServices = [];
-var globalNeutralServices = [];
-var globalWorkingIPs = [];
-var globalNotWorkingIPs = [];
-var globalImportantNames = ["MainAD", "FalloverAD", "CDN", "Backup Server"];
-var globalImportantDownNames = [];
-
-var masterUser;
-var channelID;
-
-if(env=="prod"){
-    channelID="776714951930281994";
-    masterUser="194913844190117888";
+if(process.env.PROD == "true"){
+    var channelID="776714951930281994";
+    var masterUser=["194913844190117888", "208473993374597120"];
+    console.log("Running in Production Mode");
 }else{
-    channelID="775252557990723604"
-    masterUser="205911447186702336";
+    var channelID="796677956046159872";
+    var masterUser=["205911447186702336"];
+    console.log("Running in Testing Mode");
 }
 
-var globalImportantNames=["MainAD", "FalloverAD", "CDN", "Backup Server"];
+// Load main JSON file
+const mainJSON = JSON.parse(fs.readFileSync('./main.json'));
+const vmlist=mainJSON.vms;
+const sitelist=mainJSON.websites;
+
+// Other variables
+const interval = 15;
 
 function getADate(){
-    datetime = new Date();
-    da=datetime.getDate();
-    mo=datetime.getMonth() + 1;
-    ye=datetime.getFullYear();
-    hr=datetime.getHours();
-    mn=datetime.getMinutes();
-    sc=datetime.getSeconds();
-  
-    hr=hr.toString();
-    mn=mn.toString();
-    sc=sc.toString();
+    let datetime = new Date();
+    let da=datetime.getDate();
+    let mo=datetime.getMonth() + 1;
+    let ye=datetime.getFullYear();
+    let hr=datetime.getHours().toString();
+    let mn=datetime.getMinutes().toString();
+    let sc=datetime.getSeconds().toString();
   
     if(hr.length==1){hr="0"+hr}
     if(mn.length==1){mn="0"+mn}
@@ -61,144 +47,134 @@ function getADate(){
     return(`${mo}-${da}-${ye} ${hr}:${mn}:${sc}`);
 }
 
-//adds itms in array to message
-function createMessageLists(emoji, indicator, arrayToBeChecked){
-    if(arrayToBeChecked.length != 0){
-    message+="> " + emoji + "**" + indicator + ": **";
-    console.log(indicator, arrayToBeChecked);
-        for(i = 0; i < arrayToBeChecked.length; i++){
-            if (i == arrayToBeChecked.length - 1){
-                message+=arrayToBeChecked[i]+"\n"
-              }else{
-                message+=arrayToBeChecked[i]+", ";
-              }
-        }
-    }
-}
-
-function outputToDiscord(){
-    actualDate=getADate();
-    
-    //reset variables
-    message="";
-    areImportantServicesDown=false;
-    pingMasterUser=false;
-    globalImportantDownNames=[];
-
-    //set globalImportantDownServices to the intersection of globalImportantServices with notWorkingServices and notWorkingIPs
-    importantDownServices = globalImportantNames.filter(value => globalNotWorkingServices.includes(value));
-    importantDownIPs = globalImportantNames.filter(value => globalNotWorkingIPs.includes(value));
-
-    //combine importantDownServices and importantDownIPs and send it to globalImportantDownNames
-    globalImportantDownNames = importantDownServices.concat(importantDownIPs);
-    if (globalImportantDownNames != 0){areImportantServicesDown = true; pingMasterUser = true};
-
-    title="**RMT Service Status check for: "+actualDate+"**\n";
-    createMessageLists(":white_check_mark:", "Working Services", globalWorkingServices);
-    createMessageLists(":x:", "Broken Services", globalNotWorkingServices);
-    createMessageLists(":x:", "Abnormal Services", globalNeutralServices);
-    message+="\n"
-    createMessageLists(":white_check_mark:", "Alive Machines", globalWorkingIPs);
-    createMessageLists(":x:", "Dead Machines", globalNotWorkingIPs);
-
-    if (areImportantServicesDown==true){
-        message += "\n"
-        message += "> :x:**Important services that have been detected as down: **\n> "
-        for(i = 0; i < globalImportantDownNames.length; i++){
-            if (i == globalImportantDownNames.length-1){
-                message+=globalImportantDownNames[i] + "\n";
+function createLists(emoji, name, array){
+    let str="";
+    if(array.length != 0){
+        str+="" + emoji + "**" + name + ":** ";
+        for(i = 0; i < array.length; i++){
+            if (i == array.length - 1){
+                str+=array[i]+"\n";
             }else{
-                message+=globalImportantDownNames[i] + ", ";
+                str+=array[i]+", ";
             }
         }
     }
-
-    if (globalNotWorkingIPs.length >= 4){
-        pingMasterUser=true;
-        message+="\n **More than 4 machines have been detected as offline**\n";
-    }
-
-    message+="**Done**";
-
-    channel.send(new Discord.MessageEmbed()
-        .addField(title,message)
-    );
-    if(pingMasterUser==true){
-	    channel.send("<@"+masterUser+">")
-    }
+    return(str);
 }
 
-function checkIfIPisReachable(name, domain){
-    //actual thing that pings machines and makes isAlive true of false
-    ping.sys.probe(domain, function(isAlive){
-        console.log(domain, isAlive);
-        if(isAlive==true){
-          globalWorkingIPs.push(name);
-        }else if(isAlive==false){
-          globalNotWorkingIPs.push(name);
-        }
-      });
-}
-
-function checkIfIPsAreReachable(){
-    for (i = 0; i < ipJSON.length; i++){
-
-        name = ipJSON[i].name;
-        domain = ipJSON[i].domain;
-        checkIfIPisReachable(name, domain);
-        
-        if(i == ipJSON.length -1){
-            setTimeout(outputToDiscord,10*1000)
-        }
-    }
-}
-
-function checkIfServiceIsRunning(name, domain, port){
-    //Get http code of domain and pass to if statement to determine which array to push name to
-    try{
-        https.get({host: domain}, function(res){
+function getCode(domain){
+    var promise = new Promise(function(resolve) {
+        var req = https.get({host: domain}, function(res){
             if(res.statusCode == 200 || res.statusCode == 301 || res.statusCode == 302){ //kanboard for some reason reports a code 302
-              globalWorkingServices.push(name);
+                resolve("good");
             }
             else if(res.statusCode == 404){
-              globalNotWorkingServices.push(name); 
+                resolve("bad");
             }else{
-              globalNeutralServices.push(name);
-              console.log(name, "reported", res.statusCode);
+                resolve("abnormal");
             }
-          });
-    }catch{
-        console.log("An https GET request for " + name + " has timed out.")
-    }
+        });
+        req.on('error', function(err){
+            resolve("error");
+        });
+        req.end();
+    });
+    return promise;
 }
 
-function checkIfServicesAreRunning(){
-    //reset all variables to default
-    globalWorkingServices = [];
-    globalNotWorkingServices = [];
-    globalNeutralServices = [];
-    globalWorkingIPs = [];
-    globalNotWorkingIPs = [];
+function pingMachine(domain){
+    var promise = new Promise(function(resolve) {
+        ping.sys.probe(domain, function(isAlive){
+            if(isAlive==true){
+                resolve("good");
+            }else{
+                resolve("bad");
+            }
+        });
+    });
+    return promise;
+}
 
-    for (i = 0; i < servicesJSON.length; i++){
+async function main(){
+    let date = getADate();
+    let title="**RMT Service Status check for: " + date + "**\n";
+    let message = "";
+    let pingMaster = false;
+    let goodVMs = [];
+    let badVMs = [];
+    let goodSites = [];
+    let weirdSites = [];
+    let badSites = [];
 
-        //get parameters from JSON
-        name = servicesJSON[i].name;
-        domain = servicesJSON[i].domain;
-        port = servicesJSON[i].port;
-        checkIfServiceIsRunning(name, domain, port);
+    for(i = 0; i < vmlist.length; i++){
+        workingVM=vmlist[i];
+        status = await pingMachine(workingVM.domain);
+        if(status == "good"){goodVMs.push(workingVM.name)}
+        else(badVMs.push(workingVM.name))
+    }
 
-        if(i == servicesJSON.length - 1){
-            checkIfIPsAreReachable();
+    for(o = 0; o < sitelist.length; o++){
+        workingSite=sitelist[o];
+        status = await getCode(workingSite.domain);
+        if(status == "good"){goodSites.push(workingSite.name)}
+        else if(status == "abnormal"){weirdSites.push(workingSite.name)}
+        else if(status == "bad"){badSites.push(workingSite.name)}
+    }
+
+    if(badVMs.length > 0 || badSites.length > 0 || weirdSites.length > 0){
+        pingMaster = true;
+    }
+
+    message+= createLists(":white_check_mark:", "Working Sites", goodSites);
+    message+= createLists(":x:", "Broken Sites", badSites);
+    message+= createLists(":x:", "Abnormal Sites", weirdSites);
+    message+="\n";
+    message+= createLists(":white_check_mark:", "Alive Machines", goodVMs);
+    message+= createLists(":x:", "Dead Machines", badVMs);
+    message+="**Done**"
+
+    channel.send(new Discord.MessageEmbed()
+        .addField(title, message)
+    );
+
+    if(pingMaster==true){
+        for(i = 0; i < masterUser.length; i++){
+            channel.send("<@"+masterUser[i]+">");
         }
     }
+    // Add rows to database
+    let options = [date, goodVMs.join(', '), badVMs.join(', '), goodSites.join(', '), weirdSites.join(', '), badSites.join(', ')];
+    var promise = new Promise(function(resolve, reject) {
+        db.run(`INSERT INTO bot(date, goodVMs, badVMs, goodSites, weirdSites, badSites) VALUES(?,?,?,?,?,?)`, options , function(err) {
+            if(err) {
+                console.log(err.message);
+                resolve(false);
+            }else{
+                console.log(`A row has been inserted with rowid ${this.lastID}`);
+                resolve(true);
+            }
+        });
+    });
+    return promise;
 }
 
-client.on('ready', () => {
-    channel = client.channels.cache.get(channelID);
-    checkIfServicesAreRunning();
-    setInterval(checkIfServicesAreRunning, intervalInSeconds*1000);
-    
+client.on('ready', async () => {
+    channel = await client.channels.cache.get(channelID);
+    main();
+    setInterval(main, interval*60*1000);
 })
 
 client.login(token);
+
+//sqlite
+let db = new sqlite3.Database(`./bot.db`, (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log(`Connected to database`);
+});
+
+db.serialize(function() {
+    db.run("PRAGMA cipher_compatibility = 4");
+    db.run('CREATE TABLE IF NOT EXISTS bot(date text, goodVMs text, badVMs text, goodSites text, weirdSites text, badSites text)');
+});
